53ac997768c003aceec98a2ff7df3e80b59f0848
Jeonghyun Min <20122791@tafe.wa.edu.au>
2024-09-02 13:37:19 +0800

    Add wrong test

--- examples.py
+++ examples.py
@@ -1,3 +1,4 @@
+import pytest
 def add(a, b):
     return a + b
 
@@ -5,6 +6,8 @@ def add(a, b):
 def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
+    with pytest.Raise(ValueError):
+        add(1,'1')
 
 
 def subtract(a, b):
