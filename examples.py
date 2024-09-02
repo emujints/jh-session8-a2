f389b3b4280018be4fe32c58ad00ebd152eb3a3d
Emujin <20126220@tafe@wa.edu.au>
2024-09-02 16:56:59 +0800

    restore function subtract; fixes #1

--- examples.py
+++ examples.py
@@ -2,19 +2,15 @@ import pytest
 def add(a, b):
     return a + b
 
-
 def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
-    with pytest.Raise(ValueError):
+    with pytest.raises(TypeError):
         add(1,'1')
-
 
 def subtract(a, b):
     return a - b
 
-def test_subtract(a, b):
+def test_subtract():
     assert subtract(2, 3) == -1
-# uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+
